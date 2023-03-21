8515276617b19f40ac691ba099c49a2acc92fc0e
Francis Stokes <stokesie14@gmail.com>
2023-03-21 12:47:00 +0100

    Episode 2: Setup the systick timer mechanism

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,8 +1,22 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/vector.h>
 
-#define LED_PORT (GPIOA)
-#define LED_PIN  (GPIO5)
+#define LED_PORT      (GPIOA)
+#define LED_PIN       (GPIO5)
+
+#define CPU_FREQ      (84000000)
+#define SYSTICK_FREQ  (1000)
+
+volatile uint64_t ticks = 0;
+void sys_tick_handler(void) {
+  ticks++;
+}
+
+static uint64_t get_ticks(void) {
+  return ticks;
+}
 
 static void rcc_setup(void) {
   rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
@@ -13,19 +27,27 @@ static void gpio_setup(void) {
   gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
 }
 
-static void delay_cycles(uint32_t cycles) {
-  for (uint32_t i = 0; i < cycles; i++) {
-    __asm__("nop");
-  }
+static void systick_setup(void) {
+  systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
+  systick_counter_enable();
+  systick_interrupt_enable();
 }
 
 int main(void) {
   rcc_setup();
   gpio_setup();
 
+  systick_setup();
+
+  uint64_t start_time = get_ticks();
+
   while (1) {
-    gpio_toggle(LED_PORT, LED_PIN);
-    delay_cycles(84000000 / 4);
+    if (get_ticks() - start_time >= 100) {
+      gpio_toggle(LED_PORT, LED_PIN);
+      start_time = get_ticks();
+    }
+
+    // Do useful work
   }
 
   // Never return
---
+++ plan.md
@@ -0,0 +1,16 @@
+# Episode 2 :: Deeper dive
+
+## The primary sources of information
+
+- The datasheet
+- The reference manual
+
+## What do these functions do?
+
+- Explore whats going on in the libopencm3 functions, like `gpio_mode_setup`
+
+## Better timing: Systick
+
+- Why peripherals are awesome
+- What are interrupts, and what is the interrupt vector table?
+- Configuring the systick to automate our time keeping
